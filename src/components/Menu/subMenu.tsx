8f840c91b76b04f03a922eba41dc5535a4027de7
wuchengliang <924507542@qq.com>
2022-12-27 22:31:54 +0800

    添加默认打开功能

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,10 +1,10 @@
-import React, { useContext, FunctionComponentElement } from "react";
+import React, { useContext, FunctionComponentElement, useState } from "react";
 import classNames from "classnames";
 import { MenuContext } from "./menu";
 import { MenuItemProps } from "./menuItem";
 
 export interface SubMenuProps {
-  index?: number;
+  index?: string;
   title: string;
   className?: string;
 }
@@ -16,25 +16,62 @@ const SubMenu: React.FC<SubMenuProps> = ({
   className,
 }) => {
   const context = useContext(MenuContext);
+    const openedSubMenus = context.defaultOpenSubMenus as Array<string>
+  const isOpend = (index && context.mode === 'vertical') ? openedSubMenus.includes(index) : false
+  const [ menuOpen, setOpen ] = useState(isOpend)
   const classes = classNames("menu-item submenu-item", className, {
     "is-active": context.index === index,
   });
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setOpen(!menuOpen);
+  };
+  let timer: any;
+  const handleMouse = (e: React.MouseEvent, toggled: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
+    timer = setTimeout(() => {
+      setOpen(toggled);
+    }, 300);
+  };
+  const clickEvents =
+    context.mode === "vertical"
+      ? {
+          onClick: handleClick,
+        }
+      : {};
+  const hoverEvents =
+    context.mode !== "vertical"
+      ? {
+          onMouseEnter: (e: React.MouseEvent) => {
+            handleMouse(e, true);
+          },
+          onMouseLeave: (e: React.MouseEvent) => {
+            handleMouse(e, false);
+          },
+        }
+      : {};
   const renderChildren = () => {
+    const subMenuClasses = classNames("viking-submenu", {
+      "menu-opened": menuOpen,
+    });
     const childrenComponent = React.Children.map(children, (child, i) => {
       const childElement = child as FunctionComponentElement<MenuItemProps>;
       if (childElement.type.displayName === "MenuItem") {
-        return childElement;
+        return React.cloneElement(childElement, { index: `${index}-${i}` });
       } else {
         console.error(
           "Warning: SubMenu has a child which is not a MenuItem component"
         );
       }
     });
-    return <ul className="viking-submenu">{childrenComponent}</ul>;
+    return <ul className={subMenuClasses}>{childrenComponent}</ul>;
   };
   return (
-    <li key={index} className={classes}>
-      <div className="submenu-title">{title}</div>
+    <li key={index} className={classes} {...hoverEvents}>
+      <div className="submenu-title" {...clickEvents}>
+        {title}
+      </div>
       {renderChildren()}
     </li>
   );
